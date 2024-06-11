cb9e9e30afc3add83cfb3be680bbc65a8e932245
Ajayj12 <ajaykmrrdy@gmail.com>
2024-06-11 20:01:40 +0530

    Initial commit

---
+++ src/main/java/com/example/dev2/Dev2Application.java
@@ -0,0 +1,13 @@
+package com.example.dev2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dev2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Dev2Application.class, args);
+	}
+
+}
